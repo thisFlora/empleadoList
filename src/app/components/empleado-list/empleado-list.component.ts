7510200d208aa31528ca0547b1f2eb340d90cef1
Florencia Dubininkas <florencia.dks@gmail.com>
2021-05-01 21:30:45 -0300

    Update empleadoList

---
+++ src/app/components/empleado-list/empleado-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Empleado } from 'src/app/models/Empleado';
+
+@Component({
+  selector: 'app-empleado-list',
+  templateUrl: './empleado-list.component.html',
+  styleUrls: ['./empleado-list.component.css']
+})
+export class EmpleadoListComponent implements OnInit {
+  
+  listEmpleados: Empleado[] = [
+    {legajo: 1, nombre: 'Juan', apellido: 'Perez', sexo: 'Masculino', salario: 25000},
+    {legajo: 2, nombre: 'Marcos', apellido: 'Gonzalez', sexo: 'Masculino', salario: 65000},
+    {legajo: 3, nombre: 'Marta', apellido: 'Garcia', sexo: 'Femenino', salario: 75000},
+    {legajo: 4, nombre: 'Ignacio', apellido: 'Cortes', sexo: 'Masculino', salario: 55000},
+    {legajo: 5, nombre: 'Maria', apellido: 'Navarro', sexo: 'Femenino', salario: 80000},
+    {legajo: 6, nombre: 'Joaquin', apellido: 'Marquez', sexo: 'Masculino', salario: 80000}
+  ]
+
+  radioButtonSeleccionado = 'Todos';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  obtenerTotalEmpleados(): number{
+    return this.listEmpleados.length;
+  }
+
+  obtenerTotalFemeninos(): number{
+    return this.listEmpleados.filter(list => list.sexo === 'Femenino').length;
+  }
+
+  obtenerTotalMasculinos(): number{
+    return this.listEmpleados.filter(list => list.sexo === 'Masculino').length;
+  }
+
+  empleadoCountRadioButtonChange(radioButtonSelec: string): void{
+    this.radioButtonSeleccionado = radioButtonSelec;
+  }
+}
